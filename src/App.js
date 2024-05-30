f56a1193f484f89e1f3177ea9be4d84f8134a915
tawseef <tawseefalam1@gmail.com>
2024-05-30 17:10:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routes = require("./routes/routes");
+const helmet = require("helmet");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+app.use(helmet());
 
-export default App;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors());
+app.options("*", cors());
+
+
+app.use("/v1", routes);
+
+
+
+module.exports = app;
